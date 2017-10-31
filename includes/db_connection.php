87acce863743df52d17772984bcb963eb3abb731
Adam Harpur <adam@adamharpur.com>
2017-10-31 17:07:49 -0500

    close with config

--- includes/db_connection.php
+++ includes/db_connection.php
@@ -1,28 +1,24 @@
 <?php
-// including the configuration
-$config = include(__DIR__.'/config.inc.php');
-
 class Db{
-    // using the singleton pattern we initially set the instance to null 
-    private static $link = null;
+  // using the singleton pattern we initially set the instance to null 
+  private static $link = null;
+  private static $dsn;
+  private static $username;
+  private static $password;
 
-//     //instance vars
-//     protected $db_name;
-//     protected $host;
-//     protected $username;
-//     protected $password;
-//     protected $dev_build;
+  //build the singleton
+  private function __construct($file = 'config.ini'){
+      $file = __DIR__ . '/' . $file;
+      
+      if (!$settings = parse_ini_file($file, TRUE)) throw new exception('Unable to open ' . $file . '.');
+      
 
-//   //build the singleton
-//   private function __construct($config){
-//     $this->$db_name = $config['db_name'];
-//     $this->$host = $config['host'];
-//     $this->$username = $config['username'];
-//     $this->$password = $config['password'];
-//     $this->$dev_build = $config['dev_build'];
-//    }
-   
-
+      $dsn = $settings['db']['driver'] .
+        '/host=' . $settings['db']['host'] .
+        ((!empty($settings['db']['port'])) ? (';port=' . $settings['db']['port']) : '') .
+        ';dbname=' . $settings['db']['schema'];
+   }
+    
     // private function __clone(){};
 
     public static function getInstance(){
@@ -30,10 +26,11 @@ class Db{
         // The Scope Resolution Operator (also called Paamayim Nekudotayim) or in simpler terms, the double colon, 
         //is a token that allows access to static, constant, and overridden properties or methods of a class.
 
+
         if(!isset(self::$link)){
             // note that this is a dev configuration and will throw exceptions at us ( which is good during development)
             $pdo_options[PDO::ATTR_ERRMODE] = PDO::ERRMODE_EXCEPTION;
-            self::$link = new PDO('mysql:host=localhost;dbname=final_project', 'root', '', $pdo_options);
+            self::$link = new PDO(self::$dsn, "root", '', $pdo_options);
       }
         return self::$link;
     }
