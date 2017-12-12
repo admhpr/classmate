7f7dc3555ecbd1aa176c49e0afcc01741d8cd569
Richard Jones <Jonesr3@students.westerntc.edu>
2017-12-12 16:25:47 -0600

    update config file

--- config.php
+++ config.php
@@ -17,6 +17,6 @@ if($_SERVER['SERVER_NAME'] == "localhost"){
     define("DB_PASS", "7bdaf8d3ca81e0e0f68cdc96c035e3da0ff2e16324647e66");
     define("DB_NAME", "final_project");
 
-    define("ROOT_PATH", "");
+    define("ROOT_PATH", "/");
     define("ROOT_URL", "classmate.slimapps.tech/");
 };
