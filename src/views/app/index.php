50a9c496f5b5a519b8068e1a97dcb850970d79bb
adam <adam@adamharpur.com>
2018-04-12 10:52:13 -0500

    chore(layout):position

--- src/views/app/index.php
+++ src/views/app/index.php
@@ -1,95 +1,98 @@
+<link rel="stylesheet" href="static/assets/landing-page/css/landing-page.min.css" />
 
 
-<html>
-	<head>
-		<link rel="stylesheet" href="static/assets/landing-page/css/landing-page.min.css" />
-		<!-- <meta name="viewport" content="width=device-width, initial-scale=1, user-scalable=no" /> -->
-		<!--[if lte IE 8]><script src="assets/js/ie/html5shiv.js"></script><![endif]-->
-		<!--[if lte IE 9]><link rel="stylesheet" href="assets/css/ie9.css" /><![endif]-->
-		<!--[if lte IE 8]><link rel="stylesheet" href="assets/css/ie8.css" /><![endif]-->
-	</head>
-	<body>
+<!-- Wrapper -->
+<div id="wrapper">
+  <!-- Banner -->
+  <section id="banner" class="major">
+    <div class="inner">
+      <header class="major">
+        <div class="has-text-centered">
+          <img src="<?php echo ROOT_PATH; ?>dist/img/logo.png" alt="ClassMate logo">
+          <h1>Welcome To ClassMate</h1>
+          <p class="lead">Leave class and still have questions? Ask your classmates, answer their questions and view other answers.</p>
+          <a class="button is-info" href="<?php echo ROOT_PATH;?>questions">Ask your classmates</a>
+        </div>
+      </header>
+    </div>
+  </section>
 
-		<!-- Wrapper -->
-			<div id="wrapper">
-				<!-- Banner -->
-					<section id="banner" class="major">
-						<div class="inner">
-							<header class="major">
-								<div class="has-text-centered">
-									<img src="<?php echo ROOT_PATH; ?>dist/img/logo.png" alt="ClassMate logo">
-									<h1>Welcome To ClassMate</h1>
-									<p class="lead">Leave class and still have questions? Ask your classmates, answer their questions and view other answers.</p>
-									<a class="button is-info" href="<?php echo ROOT_PATH;?>questions">Ask your classmates</a>
-								</div>
-							</header>
-						</div>
-					</section>
-
-				<!-- Main -->
-					<div id="main">
-						<!-- One -->
-							<section id="one" class="tiles">
-								<article>
-									<span class="image">
-										<img src="static/assets/landing-page/img/hand.jpeg" alt="" />
-									</span>
-									<header class="major">
-										<h3><a href="/questions" class="link">Ask The Right Questions</a></h3>
-									</header>
-								</article>
-								<article>
-									<span class="image">
-										<img src="static/assets/landing-page/img/desk.jpeg" alt="" />
-									</span>
-									<header class="major">
-										<h3><a href="#" class="link">Help With Answers</a></h3>
-									</header>
-								</article>
-								<article>
-									<span class="image">
-										<img src="static/assets/landing-page/img/js.jpeg" alt="" />
-									</span>
-									<header class="major">
-										<h3><a href="#" class="link">Filter by Category</a></h3>
-									</header>
-								</article>
-								<article>
-									<span class="image">
-										<img src="static/assets/landing-page/img/laptop.jpeg" alt="" />
-									</span>
-									<header class="major">
-										<h3><a href="#" class="link" target= "_blank">Advanced Topics in Programming</a></h3>
-									</header>
-								</article>
-								<article>
-									<span class="image">
-										<img src="static/assets/landing-page/img/point.jpeg" alt="" />
-									</span>
-									<header class="major">
-										<h3><a href="#" class="link">Advanced Web Development</a></h3>
-									</header>
-								</article>
-								<article>
-									<span class="image">
-										<img src="static/assets/landing-page/img/study.jpeg" alt="" />
-									</span>
-									<header class="major">
-										<h3><a href="#" class="link">Fall in love with Android Studio</a></h3>
-									</header>
-								</article>
-							</section>
-					</div>
+  <!-- Main -->
+  <div id="main">
+    <!-- One -->
+    <section id="one" class="tiles">
+      <article>
+        <span class="image">
+          <img src="static/assets/landing-page/img/hand.jpeg" alt="" />
+        </span>
+        <header class="major">
+          <h3>
+            <a href="/questions" class="link">Ask The Right Questions</a>
+          </h3>
+        </header>
+      </article>
+      <article>
+        <span class="image">
+          <img src="static/assets/landing-page/img/desk.jpeg" alt="" />
+        </span>
+        <header class="major">
+          <h3>
+            <a href="#" class="link">Help With Answers</a>
+          </h3>
+        </header>
+      </article>
+      <article>
+        <span class="image">
+          <img src="static/assets/landing-page/img/js.jpeg" alt="" />
+        </span>
+        <header class="major">
+          <h3>
+            <a href="#" class="link">Filter by Category</a>
+          </h3>
+        </header>
+      </article>
+      <article>
+        <span class="image">
+          <img src="static/assets/landing-page/img/laptop.jpeg" alt="" />
+        </span>
+        <header class="major">
+          <h3>
+            <a href="#" class="link" target="_blank">Advanced Topics in Programming</a>
+          </h3>
+        </header>
+      </article>
+      <article>
+        <span class="image">
+          <img src="static/assets/landing-page/img/point.jpeg" alt="" />
+        </span>
+        <header class="major">
+          <h3>
+            <a href="#" class="link">Advanced Web Development</a>
+          </h3>
+        </header>
+      </article>
+      <article>
+        <span class="image">
+          <img src="static/assets/landing-page/img/study.jpeg" alt="" />
+        </span>
+        <header class="major">
+          <h3>
+            <a href="#" class="link">Fall in love with Android Studio</a>
+          </h3>
+        </header>
+      </article>
+    </section>
+  </div>
 
 
-				<!-- Footer -->
-					<!-- <footer id="footer">
+  <!-- Footer -->
+  <!-- <footer id="footer">
 						<div class="inner">
 							<ul class="icons"> -->
-								<!-- <li><a href="#" class="icon alt fa-twitter"><span class="label">Twitter</span></a></li> -->
-								<!-- <li><a href="#" class="icon alt fa-facebook"><span class="label">Facebook</span></a></li> -->
-								<!-- <li><a href="#" class="icon alt fa-instagram"><span class="label">Instagram</span></a></li> -->
-								<!-- <li><a href="https://github.com/harps116/classmate" class="icon alt fa-github"><span class="label">GitHub</span></a></li>
+  <!-- <li><a href="#" class="icon alt fa-twitter"><span class="label">Twitter</span></a></li> -->
+  <!-- <li><a href="#" class="icon alt fa-facebook"><span class="label">Facebook</span></a></li> -->
+  <!-- <li><a href="#" class="icon alt fa-instagram"><span class="label">Instagram</span></a></li> -->
+  <!-- <li><a href="https://github.com/harps116/classmate" class="icon alt fa-github"><span class="label">GitHub</span></a></li>
 								<li><a href="https://www.linkedin.com/in/adam-harpur/" class="icon alt fa-linkedin"><span class="label">LinkedIn</span></a></li>
 							</ul>
 							<ul class="copyright">
@@ -98,11 +101,7 @@
 						</div>
 					</footer> -->
 
-			</div>
+</div>
 
-			<!-- Scripts -->
-			<script src="static/assets/landing-page/js/main.js"></script>
-
-
-	</body>
-</html>
+<!-- Scripts -->
+<script src="static/assets/landing-page/js/main.js"></script>
